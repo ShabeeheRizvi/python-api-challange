efb6d1b94111ec79517d6ae7b77b69a38f6e9ef3
Shabeehe Rizvi <shabeeherizvi@Shabeehes-MBP.lan>
2020-04-15 18:49:05 -0500

    updated homework

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,6 @@
+
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "5e66254ff4d2fd7d7818d95af5339664"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDJaq8CuEUGEdfU3awr8BifQYnkk_Iyets"
